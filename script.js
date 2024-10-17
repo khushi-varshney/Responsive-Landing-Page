af267d8d0837b302bd5cb24d116e469717f52fa5
khushi varshney <anuragkhushi703@gmail.com>
2024-10-17 16:22:10 +0530

    add new features

--- script.js
+++ script.js
@@ -3,32 +3,16 @@ let main = document.querySelector('.min-h-screen');
 let about = document.querySelector('.about');
 let body=document.querySelector('body');
 let footer = document.querySelector('.footer');
-let visuality = "light";
+
 
 about.addEventListener("click",() => {
-    if (visuality == "light") {
-        footer.style.textAlign = "center";
-        popup.style.height = "20rem";
-        popup.style.marginTop = "4vw";
-        popup.style.marginLeft = "14vw";
-        popup.style.borderRadius = "20%";
-        popup.style.backgroundColor = "pink";
-        popup.style.visibility = "visible";
-        popup.style.border = "4px solid black";
-        popup.style.width = "50vw";
-        main.style.maxHeight = "1vh";
-        main.style.minHeight="1vh";
-        main.style.visibility = "hidden";
-        visuality = "dark";
-    } else{
-        visibility="light";
-        popup.style.height = "0";
-        popup.style.marginTop = "0";
-        popup.style.marginLeft = "0";
-        popup.style.borderRadius = "0";
-        popup.style.visibility = "hidden";
-        main.style.visibility = "visible";
-        main.style.maxHeight = "380vh";
-        main.style.minHeight = "150vh";
-    }
+    main.innerHTML=`
+       <div class="About">
+           <h1>About</h1>
+           <p class="pp">Enourmous Styles is a website dediated to train and help people to get to know about the fashion and styles. This website is highly engaged in bringing the understanding of 'Authenticity' when it comes to fashion.</p>
+           <p class="pp">You should carry yourself in your best and confident form. That's why, We are here for you.</p>
+           <h2 class="ppT">Thank You</h2>
+       </div>
+    `
+    main.style.minHeight="63vh";
 });
